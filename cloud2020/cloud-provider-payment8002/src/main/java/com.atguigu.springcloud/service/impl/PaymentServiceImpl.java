d462badb94717206d9f460aa5a125b7d64f4d6e6
fuoo <390983980@qq.com>
2020-07-15 17:29:47 +0800

    录入

---
+++ cloud2020/cloud-provider-payment8002/src/main/java/com.atguigu.springcloud/service/impl/PaymentServiceImpl.java
@@ -0,0 +1,22 @@
+package com.atguigu.springcloud.service.impl;
+
+import com.atguigu.springcloud.entities.Payment;
+import com.atguigu.springcloud.service.PaymentService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaymentServiceImpl implements PaymentService {
+
+
+    @Override
+    public int create(Payment payment){
+        return 1;
+    }
+
+    @Override
+    public Payment getPaymentById( Long id){
+        return new Payment( 1L,"123");
+    }
+}
+
+
